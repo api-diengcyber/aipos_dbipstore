a8efeb593f367ef8f6aa1647b8cbbdadefd8e818
apidiengcyber <api.diengcyber@gmail.com>
2024-03-04 17:14:08 +0700

    fix: tukartambahphone

--- tukarphone.sql
+++ tukarphone.sql
@@ -11,7 +11,7 @@
  Target Server Version : 100138 (10.1.38-MariaDB)
  File Encoding         : 65001
 
- Date: 04/03/2024 16:41:08
+ Date: 04/03/2024 17:13:50
 */
 
 SET NAMES utf8mb4;
@@ -29,22 +29,15 @@ CREATE TABLE `tukarphone`  (
   `id_sales` int NULL DEFAULT NULL,
   `id_orders` int NULL DEFAULT NULL,
   `id_pembelian` int NULL DEFAULT NULL,
-  `id_faktur` int NULL DEFAULT NULL,
-  `id_member` int NULL DEFAULT NULL,
-  `nama_pembeli` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
-  `alamat_pembeli` text CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL,
-  `telp_pembeli` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
-  `id_produk_jual` int NULL DEFAULT NULL,
-  `id_produk_beli` int NULL DEFAULT NULL,
-  `qty_jual` int NULL DEFAULT NULL,
-  `qty_beli` int NULL DEFAULT NULL,
-  `biaya_jual` double NULL DEFAULT NULL,
-  `biaya_beli` double NULL DEFAULT NULL,
+  `nama_penukar` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `alamat_penukar` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `no_hp_penukar` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
 
 -- ----------------------------
 -- Records of tukarphone
 -- ----------------------------
+INSERT INTO `tukarphone` VALUES (1, NULL, NULL, '04-03-2024 17:07:26', NULL, NULL, NULL, 'Test', 'Alamat', '0819231');
 
 SET FOREIGN_KEY_CHECKS = 1;
